07fc1659f21ce15d123e90eba80f67f4c1cb5512
Виталий Маляренко <>
2021-11-04 17:17:25 +0200

    Added classwork 20-21

--- src/test/java/UIDemoTests/UIBaseTest.java
+++ src/test/java/UIDemoTests/UIBaseTest.java
@@ -3,6 +3,7 @@ package UIDemoTests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -10,13 +11,15 @@ import java.time.Duration;
 
 public class UIBaseTest {
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeTest
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterTest
